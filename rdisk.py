a71979b01d3613cfb1b9b83b6d7915a797768243
Rex Fitzhugh <rexreedfitzhugh@gmail.com>
2018-03-09 08:56:29 -0600

    was gathering cumalitive values, fixed

--- rdisk.py
+++ rdisk.py
@@ -1,47 +1,37 @@
 import time, psutil, datetime
+import regulartools as rtk
 
-time_to_run = 640
-print('Collecting disk stats for ' + str(time_to_run))
-def one_min():
-    one_min_read = []
-    one_min_write = []
-    count = 0
-    while count <= 60:
-        x = psutil.disk_io_counters(perdisk=False)
-        one_min_read.append(x.read_count)
-        one_min_write.append(x.write_count)
-        time.sleep(1)
-        count += 1
-    read_avg = sum(one_min_read) / len(one_min_read)
-    write_avg = sum(one_min_write) / len(one_min_write)
-    with open('disks.plot', 'a') as loadfile:
-        index = 0
-        while index < len(one_min_read):
-            loadfile.write(str(one_min_read[index]) + ', '
-                           + str(one_min_write[index]) + '\n')
-            index += 1
-    return read_avg, write_avg
+def io_poll(poll_time):
+    io_start = psutil.disk_io_counters(perdisk=False)
+    time.sleep(poll_time)
+    io_end = psutil.disk_io_counters(perdisk=False)
+    return io_start, io_end
+
+def crunch_data():
+    disk_start, disk_end = io_poll(5)
+    read_start = disk_start.read_count
+    read_end =  disk_end.read_count
+    write_start = disk_start.write_count
+    write_end = disk_end.write_count
+    read_data = read_end - read_start
+    write_data = write_end - write_start
+    data = str(read_data) + ', ' + str(write_data) + '\n'
+    ## will add by byte counters soon
+    #total_read = rtk.make_readable(read_data)
+    #total_wrote = rtk.make_readable(write_data)
+    io_data = open('disks.plot', 'a', 1)
+    human_stats = open('io_stats.txt', 'w')
+    human_stats.write("Read ".ljust(18) + str(read_data) + '\n'
+                      + "Write".ljust(18) + str(write_data) + '\n')
+    human_stats.close()
+    io_data.write(data)
+    io_data.close()
 
 run_time = 0
-outfile = open('disks.txt', 'a', 1)
-timer = open('disks_runtime.txt', 'a', 1)
-now = datetime.datetime.now()
-hour, minute = str(now.hour), str(now.minute)
-the_time = hour + ':' + minute
-timer.write(the_time + '\n')
 while run_time < 640:
-    read_avg, write_avg = one_min()
-    read = format(read_avg, '.2f')
-    write = format(write_avg, '.2f')
-    data = read + ', ' + write + '\n'
-    outfile.write(data)
-    if run_time % 10 == 0:
-        update = time_to_run - run_time
-        print(str(update) + 'minutes remaining\n')
-    if run_time % 60 == 0:
-        now = datetime.datetime.now()
-        hour, minute = str(now.hour), str(now.minute)
-        the_time = hour + ':' + minute
-        timer.write(the_time + '\n')
+    crunch_data()
+    f = open('io_stats.txt')
+    text = f.read()
+    print(text)
+    f.close()
     run_time += 1
-outfile.close()
