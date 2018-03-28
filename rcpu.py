f8fa249faade4acf247a7343ca7f66876d6d7c91
Rex Fitzhugh <rexreedfitzhugh@gmail.com>
2018-03-28 06:56:12 -0500

    cleaned up rcpu

--- rcpu.py
+++ rcpu.py
@@ -1,7 +1,5 @@
 import time, psutil, datetime
 
-time_to_run = 640
-print('Collecting CPU stats for ' + str(time_to_run))
 def one_min():
     one_min_load = []
     count = 0
@@ -11,28 +9,13 @@ def one_min():
         time.sleep(1)
         count += 1
     one_min_avg = sum(one_min_load) / len(one_min_load)
-    with open('uptime.plot', 'a') as loadfile:
+    with open('cpuutil.plot', 'a') as loadfile:
         for load in one_min_load:
             loadfile.write(str(load) + '\n')
     return one_min_avg
 
 run_time = 0
-outfile = open('uptime.txt', 'a', 1)
-timer = open('runtime.txt', 'a', 1)
-now = datetime.datetime.now()
-hour, minute = str(now.hour), str(now.minute)
-the_time = hour + ':' + minute
-timer.write(the_time + '\n')
-while run_time < 640:
+while run_time < 3600:
     load = one_min()
-    outfile.write("%.2f" % load + '\n')
-    if run_time % 10 == 0:
-        update = time_to_run - run_time
-        print(str(update) + 'minutes remaining\n')
-    if run_time % 60 == 0:
-        now = datetime.datetime.now()
-        hour, minute = str(now.hour), str(now.minute)
-        the_time = hour + ':' + minute
-        timer.write(the_time + '\n')
+    print("average for poll period: " + str(load) + '\n')
     run_time += 1
-outfile.close()
