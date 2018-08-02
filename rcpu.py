2a5e3b206dba4d319a98e2683813e10c33c510c3
Rex Fitzhugh <rexreedfitzhugh@gmail.com>
2018-08-02 06:01:46 -0500

    added cpu frequency polling

--- rcpu.py
+++ rcpu.py
@@ -1,33 +1,49 @@
-import time, psutil, datetime
+from __future__ import print_function
+from __future__ import division
+import time, psutil, datetime, sys
 
-def one_min_poll():
+def my_poll(runtime):
     """
-    this function gathers the cpu usage in percent value
+    this function gathers the cpu usage and speed
     """
-    one_min_load = []
+    my_load = []
+    speeds = []
     count = 0
     # while the runtime is less than a minute
-    while count <= 60:
+    while count <= runtime:
         # get the cpu usage
         x = psutil.cpu_percent(interval=None, percpu=False)
         # add it to the load list
-        one_min_load.append(x)
+        my_load.append(x)
+        # get the cpu speed
+        freq = psutil.cpu_freq()
+        # i only care about the current speed
+        cpu_speed = freq.current
+        # append the speeds list
+        speeds.append(cpu_speed)
         # wait a second and increment. repeat.
         time.sleep(1)
         count += 1
     # determine the average usage for that minute
-    one_min_avg = sum(one_min_load) / len(one_min_load)
-    # dump the data into a file (60 one second)
+    cpu_avg = sum(my_load) / len(my_load)
+    # determine low / high speed for poll period
+    min_speed, max_speed = min(speeds), max(speeds)
+    # dump the data into a csv file where the first column is cpu util
+    # and the second is the current cpu speed
     with open('cpuutil.plot', 'a') as loadfile:
-        for load in one_min_load:
-            loadfile.write(str(load) + '\n')
+        for i in range(0, len(my_load)):
+            loadfile.write(str(my_load[i]) + ', ' + str(speeds[i]) + '\n')
     # return the average for that minute
-    return one_min_avg
+    # return some values
+    return cpu_avg, min_speed, max_speed
 
 # this is a simple example of how to collect data
 # for a certain period of time (480 minutes = 8 hours)
 run_time = 0
 while run_time < 480:
-    load = one_min_poll()
-    print("average for poll period: " + str(load) + '\n')
+    load, min_freq, max_freq = my_poll(5)
+    data = "average usage: " + str(round(load, 2)) + " cpu speed min/max: " +\
+    str(round(min_freq, 2)) + '/' + str(round(max_freq, 2))
+    sys.stdout.write('%s\r' % data)
+    sys.stdout.flush()
     run_time += 1
